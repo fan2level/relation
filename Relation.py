397d89c5577a94081ffce157a145dcc46f9adb6f
fan2level <fan2level@gmail.com>
2021-08-14 00:37:23 +0900

    update plot()

--- Relation.py
+++ Relation.py
@@ -7,9 +7,12 @@ class Relation(object):
     """ relation with something
     re-define class variable from subclass
     """
-    relation = 'r'              # relation
-    item = 'i'                  # item
-    name = 'n'                  # name
+    relation = 'relation'       # relation
+    item = 'item'               # item
+    name = 'name'               # name
+    colorrelation = 'lightsalmon'
+    coloritem = 'white'
+    colorname = 'white'
     def __init__(self,rel):
         if os.path.exists(rel) == False:
             raise FileNotFoundError
@@ -17,6 +20,7 @@ class Relation(object):
         tree = parse(rel)
         root = tree.getroot()
 
+        self.dot = None
         self.__relations = self.parse(root)
 
     def parse(self, xmlrelations):
@@ -24,20 +28,55 @@ class Relation(object):
         
         relations = {self.name: relation}
         for xmlitem in xmlrelations.findall(self.item):
-            if 'item' not in relations:
-                relations['item'] = list()
+            if self.item not in relations:
+                relations[self.item] = list()
             item = xmlitem.get(self.name)
-            relations['item'].append(item)
+            relations[self.item].append(item)
         for xmlrelation in xmlrelations.findall(self.relation):
-            if 'relation' not in relations:
-                relations['relation'] = list()
-            relations['relation'].append(self.parse(xmlrelation))
+            if self.relation not in relations:
+                relations[self.relation] = list()
+            relations[self.relation].append(self.parse(xmlrelation))
 
         return relations
 
     def plot(self):
-        pprint(self.relations)
-        
+        try:
+            from graphviz import Digraph
+            dot = Digraph(comment="plot relations",
+                          format="svg",
+                          encoding="utf-8",
+                          engine="neato")
+            self.dot = dot
+            dot.attr('graph', overlap='false')
+            dot.attr('node', width='.3', height='.3')
+            
+            # rname = self.relations[self.name]
+            # dot.node(rname)
+            self.plotRelation(self.relations)
+            self.dot.view()
+        except ModuleNotFoundError as e:
+            print(f"warning!!!")
+            print(f"$>pip install graphviz")
+            print()
+            pprint(self.relations)
+            
+    def plotRelation(self, relations):
+        rname = relations[self.name]
+        self.dot.attr('node', shape='box', style='filled', fillcolor=self.colorrelation)
+        self.dot.node(rname)
+        if self.item in relations:
+            for item in relations[self.item]:
+                self.dot.attr('node', shape='box', style='filled', fillcolor=self.coloritem)
+                self.dot.node(item)
+                self.dot.edge(rname, item)
+        if self.relation in relations:
+            for relation in relations[self.relation]:
+                rname2 = relation[self.name]
+                self.dot.attr('node', shape='box', style='filled', fillcolor=self.colorrelation)
+                self.dot.node(rname2)
+                self.dot.edge(rname, rname2)
+                self.plotRelation(relation)
+
     @property
     def relations(self):
         return self.__relations
@@ -46,7 +85,3 @@ if __name__ == '__main__':
     i = '1.xml'
     r = Relation(i)
     r.plot()
-    # relations = r.relations
-    # pprint(relations)
-
-    
--- RelationExample.py
+++ RelationExample.py
@@ -7,7 +7,7 @@ from Relation import Relation
 class RelationExample(Relation):
     """ relation with something
     """
-    relation = 's'              # relation
+    relation = 'r'              # relation
     item = 'stock'              # item
     name = 'n'                  # name
     def __init__(self,rel):
@@ -15,7 +15,6 @@ class RelationExample(Relation):
 
 if __name__ == '__main__':
     i = '2.xml'
-    i = '3.xml'
     r = RelationExample(i)
     r.plot()
 
